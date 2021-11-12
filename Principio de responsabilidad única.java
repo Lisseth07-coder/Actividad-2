a1171c20069b02682918adacaa8610a0e1bfc98f
Lisseth07-coder <93866200+Lisseth07-coder@users.noreply.github.com>
2021-11-11 19:49:55 -0500

    Add files via upload

---
+++ Principio de responsabilidad única.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.principioderesponabilidadunica;
+
+/**
+ *
+ * @author diana
+ */
+public class Comida
+        
+  //Información
+{
+  public String Name;
+  public String Brand;
+  public String Comida;
+ 
+  
+  
+  //Guardarla{
+  public class comidadb
+  private Comida .Comida;
+  public comidadb(comida comida){
+_comida = comida
+        
+        
+  //Enviarla
+  {
+public void Send 
+
+    
+    
